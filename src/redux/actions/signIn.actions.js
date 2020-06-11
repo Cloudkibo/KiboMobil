680beb425877e485ce90b74a36f47e9e1b9f8acc
arveenkumar55 <39855662+arveenkumar55@users.noreply.github.com>
2020-06-11 11:42:51 +0500

    Update signIn.actions.js

--- src/redux/actions/signIn.actions.js
+++ src/redux/actions/signIn.actions.js
@@ -1,7 +1,6 @@
 import callApi from '../../utility/api.caller.service'
 
 export function signIn (data, callback) {
-  console.log('call api Login')
   return (dispatch) => {
     callApi('auth/local', 'post', data, 'accounts').then(res => {
       callback(res)
