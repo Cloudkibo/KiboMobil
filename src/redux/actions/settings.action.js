26369b703e79aaede7495a29f331dbf05e863c62
anishachhatwani <i130315@nu.edu.pk>
2020-07-29 13:53:12 +0500

    zoom

--- src/redux/actions/settings.action.js
+++ src/redux/actions/settings.action.js
@@ -2,22 +2,48 @@ import * as ActionTypes from '../constants/constants'
 import callApi from '../../utility/api.caller.service'
 
 export function showcannedResponses (data) {
-    return {
-      type: ActionTypes.GET_CANNED_RESPONSES,
-      data
-    }
+  return {
+    type: ActionTypes.GET_CANNED_RESPONSES,
+    data
   }
-  
-export function loadcannedResponses () {
-    return (dispatch) => {
-      callApi('cannedResponses')
-        .then(res => {
-          if (res.status === 'success') {
-            dispatch(showcannedResponses(res.payload))
-          } else {
-            console.log('failed to fetch canned messages')
-          }
-        })
-    }
-  }
+}
 
+export function updateZoomIntegrations (data) {
+  return {
+    type: ActionTypes.UPDATE_ZOOM_INTEGRATIONS,
+    data
+  }
+}
+
+export function loadcannedResponses () {
+  return (dispatch) => {
+    callApi('cannedResponses')
+      .then(res => {
+        if (res.status === 'success') {
+          dispatch(showcannedResponses(res.payload))
+        } else {
+          console.log('failed to fetch canned messages')
+        }
+      })
+  }
+}
+
+export function getZoomIntegrations () {
+  return (dispatch) => {
+    callApi('zoom/users')
+      .then(res => {
+        dispatch(updateZoomIntegrations(res.payload ? res.payload : []))
+      })
+  }
+}
+
+export function createZoomMeeting (data, callback) {
+  return (dispatch) => {
+    callApi('zoom/meetings', 'post', data)
+      .then(res => {
+        if (callback) {
+          callback(res)
+        }
+      })
+  }
+}
--- src/redux/reducers/settings.reducer.js
+++ src/redux/reducers/settings.reducer.js
@@ -1,16 +1,19 @@
 import * as ActionTypes from '../constants/constants'
 
 const initialState = {
-  permissions: []
+  permissions: [],
+  zoomIntegrations: []
 }
 export function settingsInfo (state = initialState, action) {
   switch (action.type) {
-    
-  case ActionTypes.GET_CANNED_RESPONSES:
-  return Object.assign({}, state, {
-    cannedResponses: action.data
-  })
-
+    case ActionTypes.GET_CANNED_RESPONSES:
+      return Object.assign({}, state, {
+        cannedResponses: action.data
+      })
+    case ActionTypes.UPDATE_ZOOM_INTEGRATIONS:
+      return Object.assign({}, state, {
+        zoomIntegrations: action.data
+      })
     default:
       return state
   }
