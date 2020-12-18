634e29a68d614cd65ac6587adaca87b782830b64
Baqar <bjafri5@gmail.com>
2020-12-18 16:21:49 +0500

    changes

--- src/components/LiveChat/Chat/GetContactInfo.js
+++ src/components/LiveChat/Chat/GetContactInfo.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import { Block, Text, Button, theme, Input } from 'galio-framework'
-import {View, TouchableOpacity, StyleSheet, Dimensions, FlatList, Alert, Linking, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback} from 'react-native'
+import {
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  FlatList,
+  Alert,
+  Linking,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback
+} from 'react-native'
 import Modal from 'react-native-modal'
 import Icon from '../../../components/Icon'
 import DropDownPicker from 'react-native-dropdown-picker'
@@ -11,32 +22,32 @@ import QuickReplies from '../Messages/QuickReplies'
 const { width } = Dimensions.get('screen')
 
 class GetContactInfo extends React.Component {
-  constructor (props, context) {
+  constructor(props, context) {
     super(props, context)
     this.state = {
-        query: '',
-        message: '',
-        keyboardInputAllowed: false,
-        skipAllowed: false,
-        errorMessage: ''
+      query: '',
+      message: '',
+      keyboardInputAllowed: false,
+      skipAllowed: false,
+      errorMessage: ''
     }
     this.getCorrespondingCustomField = this.getCorrespondingCustomField.bind(this)
     this.updateQuery = this.updateQuery.bind(this)
     this.getQuickReplies = this.getQuickReplies.bind(this)
     this.getQuickReplyTitle = this.getQuickReplyTitle.bind(this)
     this.sendMessage = this.sendMessage.bind(this)
+    this.updateText = this.updateText.bind(this)
   }
 
-
-  getCorrespondingCustomField () {
+  getCorrespondingCustomField() {
     if (this.state.query === 'email') {
-      return "Email"
+      return 'Email'
     } else if (this.state.query === 'phone') {
-      return "Phone Number"
+      return 'Phone Number'
     }
   }
 
-  getQuickReplyTitle () {
+  getQuickReplyTitle() {
     if (this.state.query === 'email') {
       return "subscriber's email"
     } else if (this.state.query === 'phone') {
@@ -44,19 +55,19 @@ class GetContactInfo extends React.Component {
     }
   }
 
-  getQuickReplies () {
-    let quickReplies = [{title: this.getQuickReplyTitle()}]
+  getQuickReplies() {
+    let quickReplies = [{ title: this.getQuickReplyTitle() }]
     if (this.state.skipAllowed) {
-        quickReplies.push({title: 'skip'})
+      quickReplies.push({ title: 'skip' })
     }
     return quickReplies
   }
 
-  sendMessage (e) {
+  sendMessage(e) {
     e.preventDefault()
     if (!this.state.message) {
-        this.setState({errorMessage: 'Message is required'})
-        return
+      this.setState({ errorMessage: 'Message is required' })
+      return
     }
     this.props.setGetContactInfoModal()
     const quickReplies = [
@@ -70,119 +81,142 @@ class GetContactInfo extends React.Component {
     this.props.sendQuickReplyMessage(this.state.message, quickReplies)
   }
 
-  updateQuery (query) {
-    const updatedState = {query}
+  updateText(text) {
+    const lines = text.split('\n')
+    if (lines.length <= 3) {
+      this.setState({ message: text, errorMessage: '' })
+    }
+  }
+
+  updateQuery(query) {
+    const updatedState = { query }
     if (!query) {
       updatedState.message = ''
       updatedState.keyboardInputAllowed = false
       updatedState.skipAllowed = false
+    } else if (query === 'email') {
+      updatedState.message = 'Please provide your email'
+    } else if (query === 'phone') {
+      updatedState.message = 'Please provide your phone number'
     }
     this.setState(updatedState)
   }
 
-  render () {
+  render() {
     return (
-      <Modal isVisible={this.props.showGetContactInfoModal} onBackdropPress={this.props.setGetContactInfoModal} style={{margin: 0}}>
-            <KeyboardAvoidingView
-                behavior={Platform.OS == "ios" ? "padding" : null}
-            >
-                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-                    <Block style={{backgroundColor: 'white', padding: 20, display: 'flex'}}>
-                        {
-                            !this.state.query ?
-                            <Block center>
-                                <Button 
-                                    onPress={() => this.updateQuery('email')}
-                                    radius={10}
-                                    style={{marginVertical: 10, marginHorizontal: 16}}
-                                >
-                                    Get Email
-                                </Button>
-                                <Button 
-                                    onPress={() => this.updateQuery('phone')}
-                                    radius={10}
-                                    style={{marginVertical: 10, marginHorizontal: 16}}
-                                >
-                                    Get Phone Number
-                                </Button>
-                            </Block> : []
-                        }
-                        {
-                            this.state.query ? 
-                            <Block>      
-                                <Text h5 style={{marginVertical: 10}}>
-                                    Ask for Subscriber's {this.getCorrespondingCustomField()}
-                                </Text>
-                                <Text>
-                                    This will enable you to retrieve subscriber's {this.getCorrespondingCustomField().toLowerCase()} and store it in a custom field: "{this.getCorrespondingCustomField()}".
-                                    {"\n\n"}
-                                    {this.state.query === 'phone' && "Note: You will only be able to retrieve user's phone number if they have made their number public on Facebook. Also, this quick reply will be removed if you send another message before the user taps on it."}
-                                    {this.state.query === 'email' && 'Note: This quick reply will be removed if you send another message before the user taps on it.'}
-                                </Text>
-                                <Input
-                                    maxLength={100}
-                                    multiline
-                                    placeholder="Enter message text..."
-                                    color="black"
-                                    placeholderTextColor="gray"
-                                    style={{height: 80, alignItems: 'flex-start', paddingVertical: 5, marginTop: 10}}
-                                    value={this.state.message}
-                                    onChangeText={text => this.setState({message: text, errorMessage: ''})}
-                                />
-                                {this.state.errorMessage ?
-                                    <Text style={{color: 'red', marginHorizontal: 10, position: 'relative', bottom: 5}}>{this.state.errorMessage}</Text> : []
-                                }
-                                <CheckBox
-                                    title={"Allow Keyboard Input"}
-                                    containerStyle={{margin: 0, marginTop: 10, padding: 0, backgroundColor: 'white', borderWidth: 0}} 
-                                    checked={this.state.keyboardInputAllowed}
-                                    onPress={() => this.setState({keyboardInputAllowed: !this.state.keyboardInputAllowed})}
-                                />
-                                <CheckBox
-                                    title={"Allow Skip"}
-                                    containerStyle={{margin: 0, marginTop: 10, padding: 0, backgroundColor: 'white', borderWidth: 0}} 
-                                    checked={this.state.skipAllowed}
-                                    onPress={() => this.setState({skipAllowed: !this.state.skipAllowed})}
-                                />
-                                <Text style={{fontWeight: "bold", marginTop: 20, marginBottom: 5}}>Preview:</Text>
-                                <Block style={styles.previewBlock}>
-                                    <Block style={styles.messageCardWrapper}>
-                                        <Block style={[styles.messageCard, styles.shadow]}>
-                                            <TextComponent
-                                                linkColor='#5867dd'
-                                                text={{text: this.state.message}}
-                                            />
-                                        </Block>
-                                    </Block>
-                                    <QuickReplies buttons={this.getQuickReplies()}/>
-                                </Block>
-
-                                <Block style={{flexDirection: 'row', justifyContent: 'flex-end', marginTop: 30}}>
-                                    <Button 
-                                        shadowless
-                                        color="warning"
-                                        onPress={() => this.updateQuery('email')}
-                                        radius={10}
-                                        style={{marginVertical: 10, width: '30%', marginRight: 20}}
-                                        onPress={() => this.updateQuery('')}
-                                    >
-                                        Cancel
-                                    </Button>
-                                    <Button 
-                                        color="success"
-                                        shadowless
-                                        onPress={this.sendMessage}
-                                        radius={10}
-                                        style={{marginVertical: 10, width: '30%'}}
-                                    >
-                                        Send
-                                    </Button>
-                                </Block>
-
-                            </Block> : []
-                        }
+      <Modal
+        isVisible={this.props.showGetContactInfoModal}
+        onBackdropPress={this.props.setGetContactInfoModal}
+        style={{ margin: 0 }}
+      >
+        <KeyboardAvoidingView behavior={Platform.OS == 'ios' ? 'padding' : null}>
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <Block style={{ backgroundColor: 'white', padding: 20, display: 'flex' }}>
+              {!this.state.query ? (
+                <Block center>
+                  <Button
+                    onPress={() => this.updateQuery('email')}
+                    radius={10}
+                    style={{ marginVertical: 10, marginHorizontal: 16 }}
+                  >
+                    Get Email
+                  </Button>
+                  <Button
+                    onPress={() => this.updateQuery('phone')}
+                    radius={10}
+                    style={{ marginVertical: 10, marginHorizontal: 16 }}
+                  >
+                    Get Phone Number
+                  </Button>
+                </Block>
+              ) : (
+                []
+              )}
+              {this.state.query ? (
+                <Block>
+                  <Text h5 style={{ marginVertical: 10 }}>
+                    Ask for Subscriber's {this.getCorrespondingCustomField()}
+                  </Text>
+                  <Text>
+                    This will enable you to retrieve subscriber's {this.getCorrespondingCustomField().toLowerCase()} and
+                    store it in a custom field: "{this.getCorrespondingCustomField()}".
+                    {'\n\n'}
+                    {this.state.query === 'phone' &&
+                      "Note: You will only be able to retrieve user's phone number if they have made their number public on Facebook. Also, this quick reply will be removed if you send another message before the user taps on it."}
+                    {this.state.query === 'email' &&
+                      'Note: This quick reply will be removed if you send another message before the user taps on it.'}
+                  </Text>
+                  <Input
+                    maxLength={100}
+                    multiline
+                    placeholder='Enter message text...'
+                    color='black'
+                    placeholderTextColor='gray'
+                    style={{
+                      minHeight: 80,
+                      maxHeight: 80,
+                      alignItems: 'flex-start',
+                      paddingVertical: 5,
+                      marginTop: 10
+                    }}
+                    value={this.state.message}
+                    onChangeText={(text) => this.updateText(text)}
+                  />
+                  {this.state.errorMessage ? (
+                    <Text style={{ color: 'red', marginHorizontal: 10, position: 'relative', bottom: 5 }}>
+                      {this.state.errorMessage}
+                    </Text>
+                  ) : (
+                    []
+                  )}
+                  <CheckBox
+                    title={'Allow Keyboard Input'}
+                    containerStyle={{ margin: 0, marginTop: 10, padding: 0, backgroundColor: 'white', borderWidth: 0 }}
+                    checked={this.state.keyboardInputAllowed}
+                    onPress={() => this.setState({ keyboardInputAllowed: !this.state.keyboardInputAllowed })}
+                  />
+                  <CheckBox
+                    title={'Allow Skip'}
+                    containerStyle={{ margin: 0, marginTop: 10, padding: 0, backgroundColor: 'white', borderWidth: 0 }}
+                    checked={this.state.skipAllowed}
+                    onPress={() => this.setState({ skipAllowed: !this.state.skipAllowed })}
+                  />
+                  <Text style={{ fontWeight: 'bold', marginTop: 20, marginBottom: 5 }}>Preview:</Text>
+                  <Block style={styles.previewBlock}>
+                    <Block style={styles.messageCardWrapper}>
+                      <Block style={[styles.messageCard, styles.shadow]}>
+                        <TextComponent linkColor='#5867dd' text={{ text: this.state.message }} />
+                      </Block>
                     </Block>
-                </TouchableWithoutFeedback>
+                    <QuickReplies buttons={this.getQuickReplies()} />
+                  </Block>
+
+                  <Block style={{ flexDirection: 'row', justifyContent: 'flex-end', marginTop: 30 }}>
+                    <Button
+                      shadowless
+                      color='gray'
+                      onPress={() => this.updateQuery('email')}
+                      radius={10}
+                      style={{ marginVertical: 10, width: '30%', marginRight: 20 }}
+                      onPress={() => this.updateQuery('')}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      shadowless
+                      onPress={this.sendMessage}
+                      radius={10}
+                      style={{ marginVertical: 10, width: '30%' }}
+                    >
+                      Send
+                    </Button>
+                  </Block>
+                </Block>
+              ) : (
+                []
+              )}
+            </Block>
+          </TouchableWithoutFeedback>
         </KeyboardAvoidingView>
       </Modal>
     )
@@ -218,12 +252,12 @@ const styles = StyleSheet.create({
     borderWidth: 2
   },
   previewBlock: {
-    backgroundColor: 'white', 
-    minHeight: 100, 
+    backgroundColor: 'white',
+    minHeight: 100,
     padding: 10,
-    borderWidth: 1, 
-    borderRadius: 5, 
-    borderColor: 'lightgray',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: 'lightgray'
   },
   messageCardWrapper: {
     alignSelf: 'flex-start',
@@ -236,12 +270,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 8,
     paddingVertical: 10,
     borderRadius: 6,
-    backgroundColor: '#D6DADD',
+    backgroundColor: '#D6DADD'
   },
   shadow: {
     shadowColor: 'rgba(0, 0, 0, 0.12)',
     shadowOffset: { width: 0, height: 7 },
     shadowRadius: 20,
     shadowOpacity: 1
-  },
+  }
 })
