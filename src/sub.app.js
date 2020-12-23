9d965bc51928b559581154d1d0e9a803dce36abd
anishachhatwani <i130315@nu.edu.pk>
2020-12-23 11:43:50 +0500

    no internet

--- src/sub.app.js
+++ src/sub.app.js
@@ -2,19 +2,22 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Screens from './navigation/Screens'
 import { Platform, StatusBar, Alert } from 'react-native'
+import NetInfo from '@react-native-community/netinfo'
 import { Block } from 'galio-framework'
 
 class SubApp extends React.Component {
   /* eslint-disable */
   UNSAFE_componentWillReceiveProps (nextProps) {
   /* eslint-enable */
-    if (!nextProps.connected) {
-      Alert.alert('',
-        'You are not connected to the internet. Make sure you have an active internet connection and try again.',
-        [{ text: 'OK' }],
-        { cancelable: true }
-      )
-    }
+    NetInfo.fetch().then(state => {
+      if (!nextProps.connected && !state.isConnected && !state.isInternetReachable) {
+        Alert.alert('',
+          'You are not connected to the internet. Make sure you have an active internet connection and try again.',
+          [{ text: 'OK' }],
+          { cancelable: true }
+        )
+      }
+    })
   }
   render () {
     return (
